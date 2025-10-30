f0d12d499ad2e27da89674e8ba493176b1994fad
Christian <baizan6@gmail.com>
2025-10-30 11:40:53 +0100

    Funcionalidad del log añadida

--- IOMATRIX.h
+++ IOMATRIX.h
@@ -79,10 +79,76 @@ void guardarMatrices(const char* nombre, int m[N][N]) {
     fclose(archivo);
 }
 
+void guardar2operaciones(FILE* archivo, int mA[N][N], int mB[N][N], int mR[N][N]) {
+    fprintf(archivo, "Matriz A:\n");
+    escribirMatriz(archivo, mA);
+    fprintf(archivo, "Matriz B:\n");
+    escribirMatriz(archivo, mB);
+    fprintf(archivo, "Resultado:\n");
+    escribirMatriz(archivo, mR);
+    fprintf(archivo, "\n");
+}
 
 
+void logOperacion(Operacion operacion, int mA[N][N], int mB[N][N], int mR[N][N], int escalar)
+{
+    FILE* archivo = fopen("log.txt", "a");
+
+    switch (operacion) {
+        case SUMA:
+            fprintf(archivo, "SUMA\n");
+            guardar2operaciones(archivo, mA, mB, mR);
+            break;
+        case RESTA:
+            fprintf(archivo, "RESTA\n");
+            guardar2operaciones(archivo, mA, mB, mR);
+            break;
+        case MULTIPLICACION:
+            fprintf(archivo, "MULTIPLICACION\n");
+            guardar2operaciones(archivo, mA, mB, mR);
+            break;
+        case PRODUCTO_ESCALAR:
+            fprintf(archivo, "PRODUCTO_ESCALAR\n");
+            fprintf(archivo, "Matriz A:\n");
+            escribirMatriz(archivo, mA);
+            fprintf(archivo, "Escalar B:\n%d\n", escalar);
+            fprintf(archivo, "Resultado:\n");
+            escribirMatriz(archivo, mR);
+            fprintf(archivo, "\n");
+            break;
 
 
+        case TRASPUESTA:
+            fprintf(archivo, "TRANSPUESTA\n");
+            fprintf(archivo, "Matriz A:\n");
+            escribirMatriz(archivo, mA);
+            fprintf(archivo, "Resultado:\n");
+            escribirMatriz(archivo, mR);
+            fprintf(archivo, "\n");
+            break;
+
+        case SIMETRICA:
+            fprintf(archivo, "SIMETRICA\n");
+            fprintf(archivo, "Matriz:\n");
+            escribirMatriz(archivo, mA);
+            fprintf(archivo, "Resultado:\n%d\n", escalar);
+            fprintf(archivo, "\n");
+            break;
+
+        case CARGA_MATRIZ:
+            fprintf(archivo, "CARGA_MATRIZ\n");
+            fprintf(archivo, "Matriz:\n");
+            escribirMatriz(archivo, mA);
+            fprintf(archivo, "\n");
+            break;
+        case GUARDADO_MATRIZ:
+            fprintf(archivo, "GUARDADO_MATRIZ\n");
+            fprintf(archivo, "Matriz:\n");
+            escribirMatriz(archivo, mA);
+            fprintf(archivo, "\n");
+            break;
+    }
+}
 
 
 #endif
